be30913ba9c7ab010880ceff98e51ba19abc040c
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-12-03 17:20:02 -0300

    requesito 5

--- src/App.js
+++ src/App.js
@@ -16,10 +16,30 @@ class App extends React.Component {
     isSaveButtonDisabled: true,
   };
 
+  validation = () => {
+    const noventa = 90;
+    const duzentosedez = 210;
+    const { cardName, cardDescription, cardAttr1,
+      cardAttr2, cardAttr3, cardImage } = this.state;
+    const validatename = cardName.length > 0;
+    const validadeImg = cardImage.length > 0;
+    const validateDesc = cardDescription.length > 0;
+    const validateattr1 = cardAttr1 <= noventa && cardAttr1 >= 0;
+    const validateattr2 = cardAttr2 <= noventa && cardAttr2 >= 0;
+    const validateattr3 = cardAttr3 <= noventa && cardAttr3 >= 0;
+    const value = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
+    const vaidateValue = value <= duzentosedez;
+    const bool = (validatename && validadeImg && validateDesc
+      && validateattr1 && validateattr2 && validateattr3 && vaidateValue);
+    this.setState(({
+      isSaveButtonDisabled: !bool,
+    }));
+  };
+
   onInputChange = ({ target }) => {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState({ [name]: value });
+    this.setState({ [name]: value }, this.validation);
   };
 
   render() {
